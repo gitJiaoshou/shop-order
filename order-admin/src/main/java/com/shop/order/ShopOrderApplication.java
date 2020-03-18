019cfedbfc8320a82e42806c1e186e118e853c2e
yangkaifei <ykaifei@163.com>
2020-03-18 11:53:12 +0800

    controller配置

---
+++ order-admin/src/main/java/com/shop/order/ShopOrderApplication.java
@@ -0,0 +1,23 @@
+package com.shop.order;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @Author YKF
+ * @date 2020/3/18上午11:41
+ */
+@SpringBootApplication
+@MapperScan({"com.shop.db.order"})
+@ComponentScan(basePackages = {"com.shop.cache.*","com.shop.order"})
+@EnableDubbo(scanBasePackages  = "com.shop.order.service.impl")
+@EnableAsync
+public class ShopOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOrderApplication.class, args);
+    }
+}
---
+++ order-admin/src/main/java/com/shop/order/config/MybatisPlusConfig.java
@@ -0,0 +1,54 @@
+package com.shop.order.config;
+
+import com.baomidou.mybatisplus.extension.parsers.DynamicTableNameParser;
+import com.baomidou.mybatisplus.extension.parsers.ITableNameHandler;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.shop.utils.HeaderConstants;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * MybatisPlus插件配置
+ * @Author YKF
+ * @date 2020/3/18上午11:51
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.shop.order.db"})
+public class MybatisPlusConfig {
+    @Autowired
+    private Tables tablesName;
+    /**
+     * 动态表名
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(HttpServletRequest request) {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        DynamicTableNameParser dynamicTableNameParser = new DynamicTableNameParser();
+        dynamicTableNameParser.setTableNameHandlerMap(new HashMap<String, ITableNameHandler>(tablesName.getTableNames().size()){{
+            //涉及表集合
+            List<String> tables = tablesName.getTableNames();
+            //动态表规则 初始表名+_+appKey
+            tables.forEach(
+                    tableTitle ->{
+                        put(tableTitle,(metaObject, sql, tableName) ->{
+                            // metaObject 可以获取传入参数，这里实现你自己的动态规则
+                            // 因为metaObject太麻烦，这里根据自身的业务需求直接从头信息里面获取
+                            String appKey = request.getHeader(HeaderConstants.APP_KEY);
+                            return String.format("%s_%s", tableName, appKey);
+                        });
+                    }
+            );
+        }});
+        paginationInterceptor.setSqlParserList(Collections.singletonList(dynamicTableNameParser));
+        return paginationInterceptor;
+    }
+}

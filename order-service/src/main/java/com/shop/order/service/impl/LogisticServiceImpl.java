6198a9941c97aac257bdde09561eff7854d62f76
yangkaifei <yangkaifei@feinno.com>
2020-05-09 21:00:46 +0800

    发送快递

--- order-service/src/main/java/com/shop/order/service/impl/LogisticServiceImpl.java
+++ order-service/src/main/java/com/shop/order/service/impl/LogisticServiceImpl.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shop.db.order.LogisticMapper;
 import com.shop.entity.order.Logistic;
 import com.shop.order.service.LogisticService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author YKF
  * @date 2020/5/9下午8:34
  */
+@Service("logisticService")
 public class LogisticServiceImpl extends ServiceImpl<LogisticMapper, Logistic> implements LogisticService {
 }

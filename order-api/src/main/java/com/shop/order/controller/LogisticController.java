f0fcc38edf43eb1b4823e63767e6a2a65b002e1c
yangkaifei <yangkaifei@feinno.com>
2020-05-10 15:15:25 +0800

    根据订单查询物流信息

---
+++ order-api/src/main/java/com/shop/order/controller/LogisticController.java
@@ -0,0 +1,41 @@
+package com.shop.order.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.shop.entity.order.Logistic;
+import com.shop.entity.order.Order;
+import com.shop.order.service.LogisticService;
+import com.shop.utils.LogisticEnum;
+import com.shop.utils.OrderEnum;
+import com.shop.utils.Result;
+import com.shop.utils.StatusEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static com.shop.utils.ShopCode.SHOP_4005_INSTALL_FAIL;
+
+@RestController
+@RequestMapping("/shop_order_api/logistic/")
+public class LogisticController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogisticController.class);
+
+    @Autowired
+    private LogisticService logisticService;
+
+    /**
+     * 查询物流
+     * @param order
+     * @return
+     */
+    @GetMapping("/{order}")
+    public Result queryByOrder(@PathVariable("order") String  order) {
+        LOGGER.info("queryByOrder ", order);
+        Logistic logistic = logisticService.getOne(new QueryWrapper<Logistic>()
+                .ne("orders", order));
+        return logistic != null ? Result.success(logistic) : Result.result(SHOP_4005_INSTALL_FAIL);
+    }
+
+}
